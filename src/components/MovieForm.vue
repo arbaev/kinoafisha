b4c19733e996a93e8531896c5e6c18eef74689ce
Tim Arbaev <timbrk@gmail.com>
2023-02-28 23:41:31 +0300

    feat: add movie form

---
+++ src/components/MovieForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <q-form
+      @submit.prevent="submitForm"
+      no-error-focus
+      autocorrect="off"
+      autocapitalize="off"
+      autocomplete="off"
+      spellcheck="false"
+    >
+      <q-input outlined required v-model="movie1" label="Кино 1" />
+      <q-input outlined required v-model="movie2" label="Кино 2" />
+      <q-input outlined required v-model="movie3" label="Кино 3" />
+
+      <div class="row justify-end q-mt-lg">
+        <q-space />
+        <q-btn label="Отправить" type="submit" color="primary" />
+      </div>
+    </q-form>
+  </section>
+</template>
+
+<script>
+// import { api } from "boot/axios";
+import { emitter } from "../boot/emitter";
+// import { netError } from "helpers/ErrorHandle";
+// import notify from "helpers/notify";
+
+export default {
+  name: "MovieForm",
+  data() {
+    return {
+      movie1: "",
+      movie2: "",
+      movie3: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      emitter.emit("movieSubmitted", [this.movie1, this.movie2, this.movie3]);
+    },
+  },
+};
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,27 +1,46 @@
 <template>
   <q-page class="flex">
-    <movie-card :movie_id="movie_id" v-for="movie_id in ids" :key="movie_id" />
+    <div v-if="ids.length === 0">
+      <movie-form />
+    </div>
+    <div v-else>
+      <movie-card
+        :movie_id="movie_id"
+        v-for="movie_id in ids"
+        :key="movie_id"
+      />
+    </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import { emitter } from "boot/emitter";
 import MovieCard from "components/MovieCard.vue";
+import MovieForm from "components/MovieForm.vue";
 
 export default defineComponent({
   name: "IndexPage",
   data() {
     return {
       loading: false,
-      // ids: [672, 87, 718789],
-      ids: [10140, 1930, 277834],
+      ids: [],
     };
   },
   components: {
     MovieCard,
+    MovieForm,
   },
   created() {},
-  methods: {},
+  mounted() {
+    emitter.on("movieSubmitted", this.movieSubmitted);
+  },
+
+  methods: {
+    movieSubmitted(movie_array) {
+      this.ids = movie_array;
+    },
+  },
 });
 </script>
 <style lang="scss"></style>
