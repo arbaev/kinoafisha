ae09c1244d4799fb254f632348a34f5276c54b88
Tim Arbaev <timbrk@gmail.com>
2023-07-01 03:27:57 +0300

    feat: MovieCard is draggable

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,19 +1,36 @@
 <script>
+import { ref } from "vue";
 import MovieCard from "./MovieCard.vue";
+import draggable from "vuedraggable";
 
 export default {
   props: ["movies"],
-  components: { MovieCard },
-  setup(props) {},
+  components: { MovieCard, draggable },
+  setup(props, { emit }) {
+    const dragging = ref(false);
+
+    const recieveRemoveMovie = (movie) => {
+      // TODO: Эту цепочку эмитов надо заменить на центральное состояние
+      emit("sendRemoveMovie", movie);
+    };
+
+    return { dragging, recieveRemoveMovie };
+  },
 };
 </script>
 
 <template>
   <div>
-    movie list
-    <div v-for="movie in movies" :key="movie.id">
-      <MovieCard :movie="movie" />
-    </div>
+    <draggable
+      :list="movies"
+      item-key="id"
+      @start="dragging = true"
+      @end="dragging = false"
+    >
+      <template #item="{ element }">
+        <MovieCard :movie="element" @sendRemoveMovie="recieveRemoveMovie" />
+      </template>
+    </draggable>
   </div>
 </template>
 
