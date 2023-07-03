29ae69a17b25120de7f59d081eb1cdba8f4cf3c3
arbaev <timbrk@gmail.com>
2023-07-03 21:47:49 +0300

    refactor: app.vue and pinia store rewrite to composition api

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,3 @@
-<script>
-import { ref, watch } from "vue";
-import { useKinoAfishaStore } from "@/stores/kinoAfishaStore";
-import HeaderMain from "@/components/HeaderMain.vue";
-import MovieForm from "@/components/MovieForm.vue";
-import MovieList from "@/components/MovieList.vue";
-import getMovie from "@/composables/getMovie";
-import hideElementsForPrint from "@/composables/hideElementsForPrint";
-
-export default {
-  components: {
-    HeaderMain,
-    MovieForm,
-    MovieList,
-  },
-
-  setup() {
-    const kinoAfishaStore = useKinoAfishaStore();
-    // TODO: Обработчик ошибок
-    // TODO: Ошибка, когда отсутствует обложка (запрос taks) или фото актера (запрос 55555)
-    // TODO: Анимации добавления/удаления
-    // TODO: В хедере вместо текста выпадашки + about
-    // TODO: Вывод результаты поиска и предлагать выбор
-    // TODO: Подключить Пинию и внести туда стейты вместо эмитов
-    // TODO: Перейти на скрипт сетап, сделать рефакторинг
-    // TODO: Оформить ридми и репозиторий
-    // TODO: Задеплоить
-    const movies = ref([]);
-    const recieveMovieNameText = (text) => {
-      fetchMovie(text);
-    };
-    const { movie, error, fetchMovie } = getMovie();
-
-    watch(movie, (movieData) => {
-      // TODO: блокировка дублей фильмов. Лажово сделана: 1) надо блокировать до сетевого запроса 2) выводить уведомление о дубле
-      if (!arrayContainsObject(movies, movie)) {
-        movies.value.unshift(movieData);
-      }
-    });
-
-    const arrayContainsObject = (arr, obj) => {
-      return arr.value.some((item) => item.id === obj.value.id);
-    };
-
-    const recieveRemoveMovie = (movie) => {
-      movies.value = movies.value.filter((m) => m.id !== movie.id);
-    };
-
-    hideElementsForPrint();
-
-    return {
-      movies,
-      error,
-      recieveMovieNameText,
-      recieveRemoveMovie,
-      kinoAfishaStore,
-    };
-  },
-};
-</script>
-
 <template>
   <div>
     <div v-show="!kinoAfishaStore.isPrinting">
@@ -68,5 +7,51 @@ export default {
     <MovieList :movies="movies" @sendRemoveMovie="recieveRemoveMovie" />
   </div>
 </template>
+
+<script setup>
+import { ref, watch } from "vue";
+import { useKinoAfishaStore } from "@/stores/kinoAfishaStore";
+import HeaderMain from "@/components/HeaderMain.vue";
+import MovieForm from "@/components/MovieForm.vue";
+import MovieList from "@/components/MovieList.vue";
+import getMovie from "@/composables/getMovie";
+import hideElementsForPrint from "@/composables/hideElementsForPrint";
+
+// TODO: Обработчик ошибок
+// TODO: Ошибка, когда отсутствует обложка (запрос taks) или фото актера (запрос 55555)
+// TODO: Анимации добавления/удаления
+// TODO: В хедере вместо текста выпадашки + about
+// TODO: Вывод результаты поиска и предлагать выбор
+// TODO: Подключить Пинию и внести туда стейты вместо эмитов
+// TODO: Перейти на скрипт сетап, сделать рефакторинг
+// TODO: Оформить ридми и репозиторий
+// TODO: Задеплоить
+
+const kinoAfishaStore = useKinoAfishaStore();
+
+const movies = ref([]);
+
+const recieveMovieNameText = (text) => {
+  fetchMovie(text);
+};
+const { movie, error, fetchMovie } = getMovie();
+
+watch(movie, (movieData) => {
+  // TODO: блокировка дублей фильмов. Лажово сделана: 1) надо блокировать до сетевого запроса 2) выводить уведомление о дубле
+  if (!arrayContainsObject(movies, movie)) {
+    movies.value.unshift(movieData);
+  }
+});
+
+const arrayContainsObject = (arr, obj) => {
+  return arr.value.some((item) => item.id === obj.value.id);
+};
+
+const recieveRemoveMovie = (movie) => {
+  movies.value = movies.value.filter((m) => m.id !== movie.id);
+};
+
+hideElementsForPrint();
+</script>
 
 <style scoped></style>
--- src/stores/kinoAfishaStore.js
+++ src/stores/kinoAfishaStore.js
@@ -1,12 +1,15 @@
+import { ref } from "vue";
 import { defineStore } from "pinia";
 
-export const useKinoAfishaStore = defineStore("kinoAfisha", {
-  state: () => ({
-    isPrinting: false,
-  }),
-  actions: {
-    setIsPrinting(bool) {
-      this.isPrinting = bool;
-    },
-  },
+export const useKinoAfishaStore = defineStore("kinoAfisha", () => {
+  const isPrinting = ref(false);
+
+  const setIsPrinting = (bool) => {
+    isPrinting.value = bool;
+  };
+
+  return {
+    isPrinting,
+    setIsPrinting,
+  };
 });
